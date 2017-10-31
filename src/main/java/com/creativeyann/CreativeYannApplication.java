4c567a931e6a0fa097808d0f8c21c165255112e4
yann <yann>
2017-10-30 20:54:55 -0400

    add index.html + minimal settings

--- src/main/java/com/creativeyann/CreativeYannApplication.java
+++ src/main/java/com/creativeyann/CreativeYannApplication.java
@@ -2,19 +2,9 @@ package com.creativeyann;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
-@Controller
 public class CreativeYannApplication {
-	
-	@RequestMapping("/")
-	@ResponseBody
-	private String index() {
-		return "Work in progress ;)";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CreativeYannApplication.class, args);
