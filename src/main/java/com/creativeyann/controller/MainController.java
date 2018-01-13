601caad3273ddffda0a9227f68e09b89b23d0e34
Yann MARCOU <yann17.marcou@gmail.com>
2018-01-12 22:47:38 -0500

    update header + footer + pages

--- src/main/java/com/creativeyann/controller/MainController.java
+++ src/main/java/com/creativeyann/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.creativeyann.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.WebMvcProperties.LocaleResolver;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,27 @@ public class MainController {
 	private String appName;
 	
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("currentPage", "index");
 		return "index";
+	}
+	
+	@GetMapping("/resume")
+	public String resume(Model model) {
+		model.addAttribute("currentPage", "resume");
+		return "resume";
+	}
+	
+	@GetMapping("/projects")
+	public String projects(Model model) {
+		model.addAttribute("currentPage", "projects");
+		return "projects";
+	}
+	
+	@GetMapping("/contact")
+	public String contact(Model model) {
+		model.addAttribute("currentPage", "contact");
+		return "contact";
 	}
 
 }
