9d19594894d308ce0df80b0ab741e4c4da7971ec
Yann MARCOU <yann17.marcou@gmail.com>
2018-01-15 00:54:08 -0500

    change i18n props location

--- src/main/java/com/creativeyann/config/i18nManager.java
+++ src/main/java/com/creativeyann/config/i18nManager.java
@@ -2,8 +2,10 @@ package com.creativeyann.config;
 
 import java.util.Locale;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -19,6 +21,14 @@ public class i18nManager extends WebMvcConfigurerAdapter{
 	    slr.setDefaultLocale(Locale.ENGLISH);
 	    return slr;
 	}
+	
+	@Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 	
 	@Bean
 	public LocaleChangeInterceptor localeChangeInterceptor() {
