f49005e91b5cc4c72ce2ed7d4c13a79b721ae3da
Yann MARCOU <yann17.marcou@gmail.com>
2018-01-15 23:05:04 -0500

    reverse security logic

---
+++ src/main/java/com/creativeyann/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.creativeyann.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+        	.headers().frameOptions().sameOrigin().and()
+                .authorizeRequests()
+					//.antMatchers("/", "/resume", "/projects", "/contact").permitAll()
+					//.antMatchers("/css/**","/img/**","/resumes/**").permitAll()
+					.antMatchers("/admin/**").hasAnyRole("ADMIN")
+					//.anyRequest().authenticated()
+					.anyRequest().permitAll()
+                .and()
+                .formLogin()
+					.loginPage("/login")
+					.permitAll()
+					.and()
+                .logout()
+					.permitAll()
+					.and()
+                .exceptionHandling().accessDeniedPage("/error");
+    }
+    
+    @Value("${login.password}")
+    private String password;
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
+                .withUser("admin").password(password).roles("ADMIN");
+    }
+}
