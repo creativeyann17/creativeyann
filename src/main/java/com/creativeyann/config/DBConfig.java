bb8157846f8588d0cfd7778c083aca840aacb5d8
Yann MARCOU <yann17.marcou@gmail.com>
2018-01-16 22:05:16 -0500

    h2 console only DEV

--- src/main/java/com/creativeyann/config/DBConfig.java
+++ src/main/java/com/creativeyann/config/DBConfig.java
@@ -2,13 +2,13 @@ package com.creativeyann.config;
 
 import javax.sql.DataSource;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class DBConfig {
@@ -21,8 +21,9 @@ public class DBConfig {
     }
     
     @Bean
+    @Profile("dev")
     public ServletRegistrationBean h2servletRegistration() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+        ServletRegistrationBean registration = new ServletRegistrationBean(new org.h2.server.web.WebServlet());
         registration.addUrlMappings("/console/*");
         return registration;
     }
